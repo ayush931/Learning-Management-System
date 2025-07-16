150dadfafe2deb687596a8f1c8d740c7b609878a
username <email>
2025-07-16 20:15:09 +0530

    Added app

---
+++ lms-app/app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+
+const HomeIndex = () => {
+  return (
+    <View>
+      <Text>Hello this is the main page</Text>
+    </View>
+  )
+}
+
+export default HomeIndex;
